034aa57e721ccc8d2a8677b3e1db7b9d2356a340
Ghost-With-A-Shoe <gunsickgun7@gmail.com>
2024-03-23 00:39:47 +0100

    Better movment + cam + headbobing
    Made some better movment. dubble jump and speed and such, better smoother camera, and also head bobing

--- Game/Assets/Scripts/PlayerMovment.cs
+++ Game/Assets/Scripts/PlayerMovment.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public CameraBob cameraBob;
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    
 
     private Vector3 velocity;
     private bool isGrounded;
+    private bool canDoubleJump = false;
 
     void Update()
     {
@@ -16,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            canDoubleJump = true;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -27,7 +31,16 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        } 
+        else if (canDoubleJump == true && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            canDoubleJump = false;
         }
+
+        bool isMoving = controller.velocity.magnitude > 0.1f && isGrounded;
+        cameraBob.DoBob(isMoving);
+
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
