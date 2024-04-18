946523b4275e7189e991411f6b94d3816296c210
KevinHermansson <kevkevvan@gmail.com>
2024-04-18 14:10:50 +0200

    Gun Model added Script missing

---
+++ Game/Assets/Scripts/Gunscript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
